f39b0e1e8aecb5d91e7921dbbe65075daeafd0ca
Luis Palma <luisarnaldopalma@gmail.com>
2022-09-17 15:23:39 +0200

    BD actualizada

--- documents/castor_db.sql
+++ documents/castor_db.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:8889
--- Tiempo de generación: 10-09-2022 a las 14:16:16
--- Versión del servidor: 5.7.34
--- Versión de PHP: 7.4.21
+-- Host: 127.0.0.1:3307
+-- Generation Time: Sep 17, 2022 at 03:22 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `castor_db`
+-- Database: `castor_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `products`
+-- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
@@ -35,31 +35,31 @@ CREATE TABLE `products` (
   `photo` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `stock` int(10) NOT NULL,
   `price` decimal(10,2) UNSIGNED NOT NULL,
-  `discount` decimal(5,2) UNSIGNED NOT NULL DEFAULT '0.00',
+  `discount` decimal(5,2) UNSIGNED NOT NULL DEFAULT 0.00,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `category_id` int(10) UNSIGNED NOT NULL,
-  `logic_delete` tinyint(4) UNSIGNED NOT NULL DEFAULT '1'
+  `logic_delete` tinyint(4) UNSIGNED NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `products`
+-- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `brand`, `description`, `photo`, `stock`, `price`, `discount`, `created_at`, `updated_at`, `category_id`, `logic_delete`) VALUES
-(1, 'Arena', NULL, 'Arena fina embolsada 50kg', 'https://fotodefault.com', 500, '350.00', '0.00', NULL, NULL, 1, 1),
-(2, 'Cemento', 'Loma Negra', '50kg', 'https://fotodefault.com', 1000, '1500.00', '0.00', NULL, NULL, 1, 1),
-(3, 'Andamio', NULL, 'Cuerpo de andamio, modular', 'https://fotodefault.com', 200, '31000.00', '0.00', NULL, NULL, 2, 1),
+(1, 'Arena', NULL, 'Arena fina embolsada 50kg', '/images/products/arenaEmbolsada.webp', 500, '350.00', '0.00', NULL, NULL, 1, 1),
+(2, 'Cemento', 'Loma Negra', '50kg', '/images/products/cemento.webp', 1000, '1500.00', '0.00', NULL, NULL, 1, 1),
+(3, 'Andamio', NULL, 'Cuerpo de andamio, modular', '/images/products/andamios.webp', 200, '31000.00', '0.00', NULL, NULL, 2, 1),
 (4, 'Inodoro', 'Roca', 'Inodoro con mochila', 'https://fotodefault.com', 200, '23000.00', '0.00', NULL, NULL, 5, 1),
 (5, 'Lavatorio', 'Roca', 'Lavatorio completo', 'https://fotodefault.com', 50, '18000.00', '10.00', NULL, NULL, 5, 1),
 (6, 'Canilla', 'fv', 'Canilla para baño estilo frances', 'https://fotodefault.com', 150, '8000.00', '0.00', NULL, NULL, 4, 1),
 (7, 'Martillo neumático', 'Stanley', 'Exclusivo para uso doméstico', 'https://fotodefault.com', 15, '50000.00', '10.40', NULL, NULL, 3, 1),
-(8, 'Hormigonera', NULL, 'Exclusivo para uso doméstico', 'https://fotodefault.com', 8, '30000.00', '0.00', NULL, NULL, 3, 1);
+(8, 'Hormigonera', NULL, 'Exclusivo para uso doméstico', '/images/products/hormigonera.webp', 8, '30000.00', '0.00', NULL, NULL, 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product_category`
+-- Table structure for table `product_category`
 --
 
 CREATE TABLE `product_category` (
@@ -68,7 +68,7 @@ CREATE TABLE `product_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `product_category`
+-- Dumping data for table `product_category`
 --
 
 INSERT INTO `product_category` (`id`, `type`) VALUES
@@ -81,7 +81,7 @@ INSERT INTO `product_category` (`id`, `type`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -91,14 +91,14 @@ CREATE TABLE `users` (
   `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `photo` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL,
-  `category_id` int(10) UNSIGNED NOT NULL DEFAULT '1',
-  `logic_delete` tinyint(4) UNSIGNED NOT NULL DEFAULT '1'
+  `category_id` int(10) UNSIGNED NOT NULL DEFAULT 1,
+  `logic_delete` tinyint(4) UNSIGNED NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `photo`, `created_at`, `updated_at`, `category_id`, `logic_delete`) VALUES
@@ -112,7 +112,7 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `phot
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user_category`
+-- Table structure for table `user_category`
 --
 
 CREATE TABLE `user_category` (
@@ -121,7 +121,7 @@ CREATE TABLE `user_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `user_category`
+-- Dumping data for table `user_category`
 --
 
 INSERT INTO `user_category` (`id`, `roll`) VALUES
@@ -132,7 +132,7 @@ INSERT INTO `user_category` (`id`, `roll`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user_x_product`
+-- Table structure for table `user_x_product`
 --
 
 CREATE TABLE `user_x_product` (
@@ -140,14 +140,14 @@ CREATE TABLE `user_x_product` (
   `user_id` int(10) UNSIGNED NOT NULL,
   `product_id` int(10) UNSIGNED NOT NULL,
   `quantity` int(100) UNSIGNED NOT NULL,
-  `comments` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `comments` text CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `comment_date` datetime DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `user_x_product`
+-- Dumping data for table `user_x_product`
 --
 
 INSERT INTO `user_x_product` (`id`, `user_id`, `product_id`, `quantity`, `comments`, `comment_date`, `created_at`, `updated_at`) VALUES
@@ -160,37 +160,37 @@ INSERT INTO `user_x_product` (`id`, `user_id`, `product_id`, `quantity`, `commen
 (7, 5, 5, 0, NULL, NULL, NULL, NULL);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `products`
+-- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
 
 --
--- Indices de la tabla `product_category`
+-- Indexes for table `product_category`
 --
 ALTER TABLE `product_category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
 
 --
--- Indices de la tabla `user_category`
+-- Indexes for table `user_category`
 --
 ALTER TABLE `user_category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `user_x_product`
+-- Indexes for table `user_x_product`
 --
 ALTER TABLE `user_x_product`
   ADD PRIMARY KEY (`id`),
@@ -198,57 +198,57 @@ ALTER TABLE `user_x_product`
   ADD KEY `product_id` (`product_id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `products`
+-- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT de la tabla `product_category`
+-- AUTO_INCREMENT for table `product_category`
 --
 ALTER TABLE `product_category`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT de la tabla `user_category`
+-- AUTO_INCREMENT for table `user_category`
 --
 ALTER TABLE `user_category`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `user_x_product`
+-- AUTO_INCREMENT for table `user_x_product`
 --
 ALTER TABLE `user_x_product`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `products`
+-- Constraints for table `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`id`);
 
 --
--- Filtros para la tabla `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `user_category` (`id`);
 
 --
--- Filtros para la tabla `user_x_product`
+-- Constraints for table `user_x_product`
 --
 ALTER TABLE `user_x_product`
   ADD CONSTRAINT `user_x_product_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
