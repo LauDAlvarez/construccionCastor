af56051bf94c69aff4c91b190e02ac28fa22f60a
juanfra12345 <juanmf09@hotmail.com>
2022-10-31 19:35:21 -0300

    actualizacion carrtview

--- public/js/cartView.js
+++ public/js/cartView.js
@@ -6,24 +6,49 @@ let idItemsLocalStorage = []
 // let productPrice = document.querySelectorAll('.precio')
 
 //mostrar los productos que corresponden
-let searchBasketId = () => { basket.forEach((x) => idItemsLocalStorage.push(x.id)) }
-
+let searchBasketId = () => {basket.forEach((x) => idItemsLocalStorage.push(x.id)) }
+//funcion para solo mostrar los productos de la canasta
 updateItems = () => {
     searchBasketId()
-    // console.log(idItemsLocalStorage)
     idItemsLocalStorage.forEach(id => {
         let search = basket.find((x) => x.id === id);
-        Array.from(producto).forEach((x) => {
-            if (x.children[0].children[0].innerHTML == id) {
-                x.style.display = 'block';
-                x.children[0].children[2].children[1].innerHTML = search.item
-                // console.log(x.children[0].children[2].children[1].innerHTML)
+        Array.from(producto).forEach((product) => {
+            if (product.children[0].children[0].innerHTML == id) {
+                //agrego numero de cantidad
+                product.children[0].children[2].children[1].innerHTML = search.item
+                //modifico numero de precio
+                product.children[0].children[2].children[3].innerHTML =
+                    (product.children[0].children[2].children[1].textContent
+                        *
+                        product.children[0].children[2].children[3].textContent)
+                product.style.display = 'block';
             }
         })
     })
 }
 
 updateItems()
+//funcion para sumar todos los items de la canasta
+let calculateTotalAmount = () => {
+    searchBasketId()
+    idItemsLocalStorage.forEach(id => {
+        let search = basket.find((x) => x.id === id);
+        Array.from(producto).forEach((product) => {
+            if (product.children[0].children[0].innerHTML == id) {
+                //agrego numero de cantidad
+                product.children[0].children[2].children[1].innerHTML = search.item
+                //modifico numero de precio
+                product.children[0].children[2].children[3].innerHTML =
+                    (product.children[0].children[2].children[1].textContent
+                        *
+                        product.children[0].children[2].children[3].textContent)
+                product.style.display = 'block';
+            }
+        })
+    })
+
+}
+
 
 
 //funcion para que se actualice num de icono del carrito
@@ -32,7 +57,7 @@ let calculation = () => {
 }
 
 
-//logica de botones + y -
+//logica de boton SUMAR
 let buttonIncrement = document.querySelectorAll(".btn-der")
 
 for (var i = 0; i < buttonIncrement.length; i++) {
@@ -40,7 +65,7 @@ for (var i = 0; i < buttonIncrement.length; i++) {
 
         //id del item
         let selectedItemId = event.target.parentElement.parentElement.children[0].innerHTML
-        
+
         //busco en la basket el elemento que necesito 
         let search = basket.find((x) => x.id === parseInt(selectedItemId));
 
@@ -52,19 +77,30 @@ for (var i = 0; i < buttonIncrement.length; i++) {
         let updateItem = () => {
             selectedItemQuantity.textContent = (selectedItemQuantity.innerHTML = search.item)
         }
+        //actualizo precio del producto al hacer click
+
+        let productPrice = event.target.parentElement.children[3]
+        let updatePrice = () => {
+            let realProductPrice = productPrice.textContent/(selectedItemQuantity.textContent)
+            let finalPrice = (realProductPrice)*(selectedItemQuantity.textContent)+realProductPrice
+            event.target.parentElement.children[3].innerHTML= finalPrice
+        }
+        //funcion para el precio
+        updatePrice()
+        //funcion para la cantidad de item
         updateItem()
+        
         
         //actualizo localstorage
         localStorage.setItem("data", JSON.stringify(basket));
-        
-        
-        //actualizacion numero en el carrito
+
+
+        //actualizacion numero en el icono carrito calculation()
         calculation()
+        // updatePrices()
     });
 }
-
-
-
+//funcion boton RESTAR
 let buttonDecrement = document.querySelectorAll(".btn-izq")
 
 for (var i = 0; i < buttonDecrement.length; i++) {
@@ -80,22 +116,99 @@ for (var i = 0; i < buttonDecrement.length; i++) {
         } else {
             search.item -= 1;
         }
-
         //actualizo cantidad del producto
         let selectedItemQuantity = event.target.parentElement.children[1]
         let updateItem = () => {
             selectedItemQuantity.textContent = (selectedItemQuantity.innerHTML = search.item)
         }
+        //actualizo precio del producto al hacer click
+
+        let productPrice = event.target.parentElement.children[3]
+        let updatePrice = () => {
+            let realProductPrice = productPrice.textContent/(selectedItemQuantity.textContent)
+            // (event.target.parentElement.children[3].textContent/event.target.parentElement.children[1].textContent)*event.target.parentElement.children[1].textContent
+            let finalPrice = realProductPrice*(selectedItemQuantity.textContent-1)
+            event.target.parentElement.children[3].innerHTML= finalPrice
+            // event.target.parentElement.children[3].innerHTML = (event.target.parentElement.children[3].textContent/event.target.parentElement.children[1].textContent*event.target.parentElement.children[1].textContent)
+            
+            // console.log(finalPrice);
+            // console.log(productPrice.textContent);
+            // console.log(selectedItemQuantity.textContent - 1);
+            console.log(realProductPrice); 
+        }
+        
+        updatePrice()
         updateItem();
 
         //saco del localstorage los elementos que tienen 0
-            basket = basket.filter((x) => x.item != 0)
-            //actualizo localstorage
-            localStorage.setItem("data", JSON.stringify(basket));
-            //actualizo icono carrito
-            calculation()
+        basket = basket.filter((x) => x.item != 0)
+        //actualizo localstorage
+        localStorage.setItem("data", JSON.stringify(basket));
+        //actualizacion numero en el icono carrito calculation()
+        calculation();
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let productPrice = document.querySelectorAll('.precio')
+// let updatePrice = () => {
+//     for (let index = 0; index < productPrice.length; index++) {
+//         const element = productPrice[index];
+//         element=element.parentElement.children[1].innerHTML;
+//         // element.innerHTML = element*()
+//     }
+// }
+
 // })
 
 // let cantidad = document.querySelectorAll('.quantity')
